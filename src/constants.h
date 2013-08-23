7adc1527bd6a61ce1332b5cbcbc2830796d111ec
James Mack <james.mack@am.sony.com>
2013-08-23 16:08:09 -0700

    Update constants.h for TLB

--- src/constants.h
+++ src/constants.h
@@ -1,23 +1,19 @@
 #include <string>
-#include "grid.h"
 
-#ifndef TEITRUS_CONSTANTS_H_
-#define TEITRUS_CONSTANTS_H_
+#ifndef TLB_CONSTANTS_H_
+#define TLB_CONSTANTS_H_
 
 /* Screen Attributes */
-const int kScreenWidth        = 740;
-const int kScreenHeight       = 840;
+const int kScreenWidth        = 800;
+const int kScreenHeight       = 600;
 const int kScreenBitsPerPixel = 32;
 
 /* Strings */
-const std::string kName = "Teitrus";
+const std::string kName = "The Left Behind";
 
 const Uint32 kRedMask   = 0xff000000;
 const Uint32 kGreenMask = 0x00ff0000;
 const Uint32 kBlueMask  = 0x0000ff00;
 const Uint32 kAlphaMask = 0x000000ff;
-
-const int kTitleX = 550;
-const int kTitleY = 100;
 
 #endif
