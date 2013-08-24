973d80fc336c02b7f4da36edab997d0aec804112
James Mack <james.mack@am.sony.com>
2013-08-23 18:23:41 -0700

    Move screen constants to screen header

--- src/constants.h
+++ src/constants.h
@@ -3,13 +3,10 @@
 #ifndef TLB_CONSTANTS_H_
 #define TLB_CONSTANTS_H_
 
-/* Screen Attributes */
-const int kScreenWidth        = 800;
-const int kScreenHeight       = 600;
-const int kScreenBitsPerPixel = 32;
+
 
 /* Strings */
-const std::string kName = "The Left Behind";
+const std::string kWindowName = "The Left Behind";
 
 const Uint32 kRedMask   = 0xff000000;
 const Uint32 kGreenMask = 0x00ff0000;
