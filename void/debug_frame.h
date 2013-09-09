48c29622e3ab86bc1d8de42b9437a00fed154d99
James Mack <jdmack@ucsd.edu>
2013-09-09 14:26:57 -0700

    Create void folder for code abandonded for now
    Moved debug_frame to void since it's buggy atm

---
+++ void/debug_frame.h
@@ -0,0 +1,42 @@
+#ifndef TLB_DEBUG_FRAME_H_
+#define TLB_DEBUG_FRAME_H_
+
+#include "SDL2/SDL.h"
+#include "constants.h"
+#include "screen.h"
+
+const int kDebugFrameWidth = 200;
+const int kDebugFrameHeight = 50;
+const int kDebugFrameXPosition = kScreenWidth - kDebugFrameWidth;
+const int kDebugFrameYPosition = kScreenHeight - kDebugFrameHeight;
+const int kDebugFrameFontSize = 12;
+const int kDebugFrameBorder = 5;
+
+class DebugFrame
+{
+    private:
+        int x_position_;
+        int y_position_;
+        int width_;
+        int height_;
+        bool shown_;
+        SDL_Color fg_color_;
+
+        //TTF_Font * font_;
+        SDL_Texture * texture_;
+        SDL_Rect background_;
+        Screen * screen_;
+
+
+    public:
+        DebugFrame(Screen * screen);
+        ~DebugFrame();
+
+        // accessors
+        bool shown() const { return shown_; }
+
+        void render();
+        bool toggle();
+};
+
+#endif
