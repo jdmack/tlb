704dba7d712ba758ff820fcad5605fd08f7860ab
James Mack <jdmack@ucsd.edu>
2014-04-25 20:51:27 -0700

    Remove Screen references from Point

--- src/point.cpp
+++ src/point.cpp
@@ -1,14 +1,12 @@
 
 #include <sstream>
 #include "point.h"
-#include "constants.h"
-#include "screen.h"
 
 Point::Point()
 {
 	// Default location is center of screen
-    x_= kScreenWidth / 2;
-    y_= kScreenHeight / 2;
+    x_= 0;
+    y_= 0;
 }
 
 Point::Point(double x, double y)
