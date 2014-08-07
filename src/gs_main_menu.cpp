574a4f064de23a35626e87bdb2bf978449f916a2
James Mack <wolfbro@gmail.com>
2014-08-07 12:33:54 -0700

    Create gs_main_menu.cpp
    From work.

---
+++ src/gs_main_menu.cpp
@@ -0,0 +1,38 @@
+#include <vector>
+
+#include "gs_main_menu.h"
+
+#include "renderer.h"
+#include "game.h"
+#include "assets.h"
+#include "utils/logger.h"
+
+
+GSMainMenu::GSMainMenu(Game * game)
+{
+    game_ = game;
+}
+
+int GSLevel::init()
+{
+    int return_code = 0;
+
+
+    return return_code;
+}
+
+bool GSLevel::update(int delta_ticks)
+{
+
+    return true;
+}
+void GSLevel::render()
+{
+
+}
+
+void GSLevel::end()
+{
+
+}
+
