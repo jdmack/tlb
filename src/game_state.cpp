5175ae0e2c3e077e9336eeb8ef39cef4991a50ad
James Mack <wolfbro@gmail.com>
2014-08-07 12:32:55 -0700

    Create game_state.cpp
    From work.

---
+++ src/game_state.cpp
@@ -0,0 +1,30 @@
+#include "game_state.h"
+
+GameState::GameState()
+{
+}
+
+GameState::~GameState()
+{
+
+}
+
+int GameState::init()
+{
+    return 0;
+}
+
+bool GameState::update(int delta_ticks)
+{
+    return true;
+}
+
+void GameState::render()
+{
+
+}
+
+void GameState::end()
+{
+
+}
