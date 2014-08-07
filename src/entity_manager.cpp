9b970e1b1b5497493085c942224fa0332eab7e10
James Mack <wolfbro@gmail.com>
2014-08-07 12:31:22 -0700

    Update entity_manager.cpp
    From work.

--- src/entity_manager.cpp
+++ src/entity_manager.cpp
@@ -1,123 +1,198 @@
-#include <vector>
 
-#include "gs_level.h"
-
+#include "entity_manager.h"
 #include "renderer.h"
 #include "game.h"
-#include "level.h"
-#include "entity_manager.h"
-#include "actions/zombie_action.h"
-#include "entity.h"
-#include "assets.h"
+#include "game_object.h"
 #include "utils/logger.h"
+#include "point.h"
+#include "entity.h"
 
-
-GSLevel::GSLevel(Game * game)
+////////////////////////////////////////////////////////////////////////////////
+//
+//
+//
+////////////////////////////////////////////////////////////////////////////////
+EntityManager::EntityManager()
 {
-    game_ = game;
+    objects_ = new std::list<GameObject *>();
+    selected_ = new std::list<GameObject *>();
 }
 
-int GSLevel::init()
+////////////////////////////////////////////////////////////////////////////////
+//
+//
+//
+////////////////////////////////////////////////////////////////////////////////
+void EntityManager::add_object(GameObject * object)
 {
-    int return_code = 0;
-
-    // Load level
-    Level * level = new Level(game_);
-
-    if(!level->load(kMapTest24x18)) {
-        Logger::write(Logger::string_stream << "Failed to load map");
-        return_code = 1;
-    }
-
-    game_->set_level(level);
-
-    Entity * char1 = spawn_entity(PLAYER, Point(48 * 6 + 24, 48 * 4 + 24), 90);
-    Entity * zombie1 = spawn_entity(ZOMBIE, Point(48 * 4 + 24, 48 * 1 + 24), 180);
-    Entity * zombie2 = spawn_entity(ZOMBIE, Point(48 * 5 + 24, 48 * 1 + 24), 90);
-    Entity * zombie3 = spawn_entity(ZOMBIE, Point(48 * 6 + 24, 48 * 1 + 24), 90);
-    Entity * zombie4 = spawn_entity(ZOMBIE, Point(48 * 7 + 24, 48 * 1 + 24), 90);
-    Entity * zombie5 = spawn_entity(ZOMBIE, Point(48 * 8 + 24, 48 * 1 + 24), 0);
-    //Entity * zombie6 = spawn_entity(ZOMBIE, Point(48 * 7 + 24, 48 * 1 + 24), 0);
-
-    return return_code;
+    objects_->push_back(object);
 }
 
-bool GSLevel::update(int delta_ticks)
+////////////////////////////////////////////////////////////////////////////////
+//
+//
+//
+////////////////////////////////////////////////////////////////////////////////
+void EntityManager::remove_object(GameObject * object)
 {
-    std::vector<Entity *> entities = game_->entity_manager()->get_entities();
-
-    // Update
-    for(std::vector<Entity *>::iterator entity_iterator = entities.begin(); entity_iterator != entities.end(); ++entity_iterator) {
-        (*entity_iterator)->update(delta_ticks);
-    }
-
-
-    return true;
-}
-void GSLevel::render()
-{
-    game_->level()->render();
-
-    // Render
-    std::vector<Entity *> entities = game_->entity_manager()->get_entities();
-    for(std::vector<Entity *>::iterator entity_iterator = entities.begin(); entity_iterator != entities.end(); ++entity_iterator) {
-        (*entity_iterator)->render();
-
-        if((*entity_iterator)->type() == PLAYER) {
-            game_->renderer()->draw_life_bar(*entity_iterator);
+    std::list<GameObject *>::iterator object_iterator = objects_->begin();
+    while(object_iterator != objects_->end()) {
+        if(object == *object_iterator) {
+            object_iterator = objects_->erase(object_iterator);
+        }
+        else {
+            object_iterator++;
         }
     }
 }
 
-void GSLevel::end()
+////////////////////////////////////////////////////////////////////////////////
+//
+//
+//
+////////////////////////////////////////////////////////////////////////////////
+void EntityManager::delete_object(GameObject * object)
 {
-    game_->entity_manager()->delete_all();
-    // TODO(2014-08-06/JM): Move Level into this class
-    // TODO(2014-08-06/JM): Delete Level
-}
+    std::list<GameObject *>::iterator object_iterator = objects_->begin();
+    while(object_iterator != objects_->end()) {
 
-Entity * GSLevel::spawn_entity(EntityType type, Point position, double rotation)
-{
-    Entity * entity = new Entity(game_, type, position, rotation);
-    if(type == PLAYER) {
-        entity->set_controllable(true);
-        entity->create_sprite(kAssetSpriteHuman1);
-    }
-    else if(type == HUMAN) {
-        entity->set_controllable(false);
-        entity->create_sprite(kAssetSpriteHuman1);
-    }
-    else if(type == ZOMBIE) {
-        entity->set_selectable(false);
-        entity->set_controllable(false);
-        entity->set_maximum_speed(kEntityDefaultVelocity - 10);
-
-        int random_num = rand() % 5 + 1;
-
-        std::string asset;
-        switch(random_num) {
-            case 1: asset = kAssetSpriteZombie1; break;
-            case 2: asset = kAssetSpriteZombie2; break;
-            case 3: asset = kAssetSpriteZombie3; break;
-            case 4: asset = kAssetSpriteZombie4; break;
-            case 5: asset = kAssetSpriteZombie5; break;
-            case 6: asset = kAssetSpriteZombie6; break;
-            default: asset = kAssetSpriteZombie1; break;
+        if(object == *object_iterator) {
+            object_iterator = objects_->erase(object_iterator);
+            delete object;
         }
-        entity->create_sprite(asset);
-
-        ZombieAction * zombie_action = new ZombieAction();
-        zombie_action->set_entity_manager(game_->entity_manager());
-        zombie_action->set_game(game_);
-
-        entity->set_current_action(zombie_action);
+        else {
+            object_iterator++;
+        }
     }
-
-    game_->entity_manager()->add_object(entity);
-    game_->renderer()->init_object(entity);
-
-
-    return entity;
 }
 
+////////////////////////////////////////////////////////////////////////////////
+//
+//
+//
+////////////////////////////////////////////////////////////////////////////////
+void EntityManager::delete_all()
+{
+    deselect_all();
 
+    std::list<GameObject *>::iterator object_iterator = objects_->begin();
+    while(object_iterator != objects_->end()) {
+
+        GameObject * object = *object_iterator;
+        object_iterator = objects_->erase(object_iterator);
+        delete object;
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//
+//
+////////////////////////////////////////////////////////////////////////////////
+GameObject * EntityManager::get_object_at(double x, double y)
+{
+    for(std::list<GameObject *>::iterator object_iterator = objects_->begin(); object_iterator != objects_->end(); ++object_iterator) {
+        if((*object_iterator)->contains_point(x, y)) {
+            return *object_iterator;
+        }
+    }
+
+    return nullptr;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//
+//
+////////////////////////////////////////////////////////////////////////////////
+std::vector<Entity *> EntityManager::get_entities()
+{
+    std::vector<Entity *> entities;
+
+    for(std::list<GameObject *>::iterator object_iterator = objects_->begin(); object_iterator != objects_->end(); ++object_iterator) {
+        GameObject * object = *object_iterator;
+        if(object->is_entity()) {
+            Entity * entity = static_cast<Entity *>(object);
+            entities.push_back(entity);
+        }
+    }
+
+    return entities;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//
+//
+////////////////////////////////////////////////////////////////////////////////
+std::vector<Entity *> EntityManager::get_entities_near(Point position, double radius)
+{
+    std::vector<Entity *> entities;
+
+    for(std::list<GameObject *>::iterator object_iterator = objects_->begin(); object_iterator != objects_->end(); ++object_iterator) {
+        GameObject * object = *object_iterator;
+        if(object->is_entity()) {
+            Entity * entity = static_cast<Entity *>(object);
+            Point p = Point(entity->x_position(), entity->y_position());
+            double distance = p.distance_from(position);
+            if(distance <= radius) {
+                entities.push_back(entity);
+            }
+        }
+    }
+
+    return entities;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//
+//
+////////////////////////////////////////////////////////////////////////////////
+void EntityManager::select(GameObject * object)
+{
+    object->select();
+    selected_->push_back(object);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//
+//
+////////////////////////////////////////////////////////////////////////////////
+void EntityManager::deselect(GameObject * object)
+{
+    object->deselect();
+    std::list<GameObject *>::iterator selected_iterator = selected_->begin();
+
+    while(selected_iterator != selected_->end()) {
+
+        if((*selected_iterator)->object_id() == object->object_id()) {
+            (*selected_iterator)->deselect();
+            selected_iterator = selected_->erase(selected_iterator);
+            break;
+        }
+        else {
+            selected_iterator++;
+        }
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//
+//
+////////////////////////////////////////////////////////////////////////////////
+void EntityManager::deselect_all()
+{
+    /*
+    std::list<GameObject *>::iterator selected_iterator = selected_->begin();
+
+    while(selected_iterator != selected_->end()) {
+
+        (*selected_iterator)->deselect();
+
+        selected_iterator = selected_->erase(selected_iterator);
+    }
+    */
+}
