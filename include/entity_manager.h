0855c40ce998bf935dd9528d759ed6d29ee55e68
James Mack <wolfbro@gmail.com>
2014-08-07 12:27:15 -0700

    Update entity_manager.h
    From Work.

--- include/entity_manager.h
+++ include/entity_manager.h
@@ -1,9 +1,9 @@
-/* Entity Agent */
 
-#ifndef TLB_ENTIGENT_H_
-#define TLB_ENTIGENT_H_
+#ifndef TLB_ENTITY_MANAGER_H_
+#define TLB_ENTITY_MANAGER_H_
 
 #include <vector>
+#include <list>
 #include "point.h"
 
 class GameObject;
@@ -12,24 +12,26 @@ class Entity;
 class EntityManager
 {
     private:
-        std::vector<GameObject *> * objects_;
-        std::vector<GameObject *> * selected_;
-
-
+        std::list<GameObject *> * objects_;
+        std::list<GameObject *> * selected_;
 
     public:
         EntityManager();
 
         // accessors
-        std::vector<GameObject *> * objects() { return objects_; }
-        std::vector<GameObject *> * selected() { return selected_; }
+        std::list<GameObject *> * objects() { return objects_; }
+        std::list<GameObject *> * selected() { return selected_; }
 
         void add_object(GameObject * object);
+        void remove_object(GameObject * object);
+        void delete_object(GameObject * object);
+        void delete_all();
         void select(GameObject * object);
         void deselect(GameObject * object);
         void deselect_all();
         GameObject * get_object_at(double x, double y);
         std::vector<Entity *> get_entities_near(Point position, double radius);
+        std::vector<Entity *> get_entities();
 };
 
 #endif
