c8a2dbbd221f8a88707ff46c292c43dc1e78573b
James Mack <jdmack@ucsd.edu>
2014-08-10 17:59:07 -0700

    Fix function name for !=

--- src/point.cpp
+++ src/point.cpp
@@ -23,7 +23,8 @@ std::string Point::to_string()
     return ss.str();
 }
 
-bool Point::operator==(const Point &other) const {
+bool Point::operator==(const Point &other) const
+{
     if((x_ == other.x()) && (y_ == other.y())) {
         return true;
     }
@@ -32,7 +33,7 @@ bool Point::operator==(const Point &other) const {
     }
 }
 
-bool Point::operator==(const Point &other) const {
+bool Point::operator!=(const Point &other) const
 {
     return !(*this == other);
 }
