4ce092d4e9e8533c667eeee4af12e726c6011d49
James Mack <wolfbro@gmail.com>
2014-08-13 12:01:10 -0700

    Create level_square.h

---
+++ include/level_square.h
@@ -0,0 +1,48 @@
+#ifndef TLB_LEVEL_SQUARE_H_
+#define TLB_LEVEL_SQUARE_H_
+
+#include <vector>
+#include <string>
+#include "SDL2/SDL.h"
+
+class Tile;
+class GameObject;
+class Camera;
+class Game;
+class Grid;
+
+class LevelSquare
+{
+    private:
+        double width_;
+        double height_;
+        int total_tiles_;
+        std::vector<Tile *> * tiles_;
+        Game * game_;
+        SDL_Texture * texture_;
+        Grid * grid_;
+
+        bool thing;
+
+    public:
+        LevelSquare(Game * game);
+
+        // accessors
+        double width() const { return width_; }
+        double height() const { return height_; }
+        int total_tiles() const { return total_tiles_; }
+        Game * game() const { return game_; }
+        SDL_Texture * texture() const { return texture_; }
+        std::vector<Tile *> * tiles() const { return tiles_; }
+        Grid * grid() const { return grid_; }
+
+        bool load(std::string filename);
+        bool touches_wall(GameObject * object, SDL_Rect * rect);
+
+        void render();
+        void build_grid();
+
+        bool is_walkable(int row, int col);
+};
+
+#endif
