5e11b90239a5842400bbc413f7a22871c41c3f38
James Mack <wolfbro@gmail.com>
2014-09-15 16:33:06 -0700

    Text defaults

--- include/ui/text.h
+++ include/ui/text.h
@@ -1,11 +1,14 @@
-#ifndef TLB_TEXT_H_
-#define TLB_TEXT_H_
+#ifndef TLB_UI_TEXT_H_
+#define TLB_UI_TEXT_H_
 
 #include "SDL2/SDL.h"
 #include "SDL2/SDL_ttf.h"
 #include "renderer.h"
 #include "color.h"
 #include "point.h"
+
+const int kDefaultTextWrap = 1000;
+const int kDefaultTextSize = 28;
 
 class Text
 {
@@ -14,8 +17,10 @@ class Text
         int width_;
         int height_;
         int size_;
+        int wrap_;
         bool visible_;
         std::string text_;
+        std::string font_asset_;
         Color color_;
 
         SDL_Texture * texture_;
@@ -25,7 +30,7 @@ class Text
 
     public:
         Text();
-        Text(std::string text);
+        Text(std::string text, int size = kDefaultTextSize);
         ~Text();
 
         // accessors
@@ -35,13 +40,17 @@ class Text
         void set_width(int width) { width_ = width; }
         void set_height(int height) { height_ = height; }
         void set_size(int size) { size_ = size; }
+        void set_wrap(int wrap) { wrap_ = wrap; }
         void set_visible(bool visible) { visible_ = visible; }
         void set_color(Color color) { color_ = color; }
+        void set_font_asset(std::string font_asset) { font_asset_ = font_asset; }
 
         void set_text(std::string text);
 
+        void update();
         void render();
         TTF_Font * load_font(std::string filename, int size);
+        void reload_font();
 };
 
 #endif
