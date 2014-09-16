3fdf2b6eec30ec1b6ebbabb6ec71c43fa72342fe
James Mack <jdmack@ucsd.edu>
2014-09-15 22:25:01 -0700

    Change Rectangle to Frame

--- include/frame.h
+++ include/frame.h
@@ -1,27 +1,24 @@
-#ifndef TLB_RECTANGLE_H_
-#define TLB_RECTANGLE_H_
+#ifndef TLB_FRAME_H_
+#define TLB_FRAME_H_
 
-#include "SDL2/SDL.h"
-
-class Rectangle
+class Frame
 {
     private:
         int x_;
         int y_;
         int width_;
         int height_;
-        SDL_Rect * sdl_rect_;
 
     public:
-        Rectangle();
-        Rectangle(int x, int y, int width, int height);
+        Frame();
+        Frame(int x, int y, int width, int height);
+        ~Frame();
 
         // accessors
         int x() const { return x_; }
         int y() const { return y_; }
         int width() const { return width_; }
         int height() const { return height_; }
-        SDL_Rect * sdl_rect() const { return sdl_rect_; }
 
         // mutators
         void set_x(int x) { x_ = x; }
@@ -29,8 +26,6 @@ class Rectangle
         void set_width(int width) { width_ = width; }
         void set_height(int height) { height_ = height; }
 
-        void set_sdl_rect();
-        void set_sdl_rect(int x, int y, int width, int height);
 };
 
 #endif
---
+++ src/frame.cpp
@@ -0,0 +1,19 @@
+#include "framframe.h"
+
+Frame::Frame()
+{
+    x_      = 0;
+    y_      = 0;
+    width_  = 0;
+    height_ = 0;
+    
+}
+
+Frame::Frame(int x, int y, int width, int height)
+{
+    x_      = 0;
+    y_      = 0;
+    width_  = 0;
+    height_ = 0;
+}
+
