4bbff6eb82bad8eaf73104c46a5ad9399f323f4b
James Mack <jdmack@ucsd.edu>
2015-05-13 00:54:32 -0700

    Add Win Condition

---
+++ include/win_condition.h
@@ -0,0 +1,16 @@
+#ifndef TLB_WIN_CONDITION_H_
+#define TLB_WIN_CONDITION_H_
+
+class WinCondition
+{
+    private:
+        bool check_enemies();
+
+    public:
+        WinCondition();
+
+        bool check();
+
+};
+
+#endif
---
+++ src/win_condition.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+
+#include "win_condition.h"
+#include "entity_manager.h"
+#include "entity.h"
+#include "game.h"
+
+WinCondition::WinCondition()
+{
+    // nothing to do
+}
+
+bool WinCondition::check()
+{
+    return check_enemies();
+}
+
+bool WinCondition::check_enemies()
+{
+    std::vector<Entity *> entities = Game::instance()->entity_manager()->get_entities();
+    for(std::vector<Entity *>::iterator it = entities.begin(); it != entities.end(); ++it) {
+        if(!(*it)->dead()) {
+            return false;
+        }
+    }
+
+    return true;
+}
