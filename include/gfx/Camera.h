606a483f31a9b1a54042dcc8780ff788b481639f
James Mack <wolfbro@gmail.com>
2016-05-09 23:24:42 -0700

    Fix indentation

--- include/gfx/Camera.h
+++ include/gfx/Camera.h
@@ -9,49 +9,50 @@
 
 class Camera
 {
-   private:
+    private:
 
-       Vector3f position_;
-       Vector3f target_;
-       Vector3f up_;
-       Matrix4f view_;
+        Vector3f position_;
+        Vector3f target_;
+        Vector3f up_;
+        Matrix4f view_;
 
-       int windowWidth_;
-       int windowHeight_;
+        int windowWidth_;
+        int windowHeight_;
 
-       float angleH_;
-       float angleV_;
+        float angleH_;
+        float angleV_;
 
-       bool onUpperEdge_;
-       bool onLowerEdge_;
-       bool onLeftEdge_;
-       bool onRightEdge_;
+        bool onUpperEdge_;
+        bool onLowerEdge_;
+        bool onLeftEdge_;
+        bool onRightEdge_;
 
-       Vector2i mousePosition_;
-       PersProjInfo persProjInfo_;
+        Vector2i mousePosition_;
+        PersProjInfo persProjInfo_;
 
-       void init();
-       void update();
+        void init();
+        void update();
 
-       void updateView();
+        void updateView();
 
-   public:
+    public:
 
-       Camera(int windowWindow, int windowHeight);
+        Camera(int windowWindow, int windowHeight);
 
-       Camera(int windowWidth, int windowHeight, const Vector3f & position, const Vector3f & target, const Vector3f & up);
+        Camera(int windowWidth, int windowHeight, const Vector3f & position, const Vector3f & target, const Vector3f & up);
 
-       bool onKeyboard(KeyType key);
+        bool onKeyboard(KeyType key);
 
-       void onMouse(int x, int y);
+        void onMouse(int x, int y);
 
-       void onRender();
+        void onRender();
 
-       Vector3f position() const { return position_; }
-       Vector3f target() const { return target_; } 
-       Vector3f up() const { return up_; }
-       Matrix4f view();//const { return view_; }
-       PersProjInfo persProjInfo() { return persProjInfo_; }
+        Vector3f position() const { return position_; }
+        Vector3f target() const { return target_; } 
+        Vector3f up() const { return up_; }
+        Matrix4f view();//const { return view_; }
+        PersProjInfo persProjInfo() { return persProjInfo_; }
+        
 
 };
 
