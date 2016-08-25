4abdf6cfeec2477a08c978cf1785eafe4dfa8979
James Mack <github@wulph.com>
2016-08-24 17:57:23 -0700

    Update Ray for Vertex class

--- src/gfx/Ray.cpp
+++ src/gfx/Ray.cpp
@@ -10,20 +10,19 @@ Ray::Ray(Vector3f direction, Vector3f origin)
     origin_ = origin;
     loadModelData();
     model_.setMode(DRAW_LINES);
+    model_.setColor(Vector4f(1.0f, 0.0f, 0.0f, 1.0f));
 }
 
 void Ray::loadModelData()
 {
-    /*
-    Vector3f vertices[2];
-    vertices[0] = origin_;
-    vertices[1] = (direction_ * 100) + origin_;
+    Vertex vertices[2];
+    vertices[0] = Vertex(origin_, Vector2f(0.0f, 0.0f));;
+    vertices[1] = Vertex((direction_ * 100) + origin_, Vector2f(0.0f, 0.0f));;
 
     int indices[] = { 0, 1 };
 
     model_.loadVertices(vertices, sizeof(vertices));
     model_.loadIndices(indices, sizeof(indices));
-    */
 }
 
 void Ray::setDirection(Vector3f direction)
